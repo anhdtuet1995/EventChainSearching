f7cae1d214ae7a24df6af58a892ebcf202c28b72
anhdtuet1995 <anhdtuet1995@gmail.com>
2019-04-28 04:56:33 +0700

    change U

--- main.js
+++ main.js
@@ -1,68 +1,95 @@
 var oauthEventbriteKey = "WY5CK4MUPSE4DQSPEDWQ";
 var numberEventInOnePage = 10;
-
+var cachingRes = [];
+var currPage = -1;
+var currEventBritePage = 0;
+var eventBritePageCount = 0;
 class EventInfo {
-  	constructor(name, location, organizer, dateFrom, dateTo, image, url) {
-    	this.name = name;
-    	this.location =  location;
-    	this.organizer =  organizer;
-    	this.dateFrom =  dateFrom;
-    	this.dateTo =  dateTo;
-    	this.image =  image;
-    	this.url =  url;
-  	}
+	constructor(name, location, organizer, dateFrom, dateTo, image, url) {
+		this.name = name;
+		this.location =  location;
+		this.organizer =  organizer;
+		this.dateFrom =  dateFrom;
+		this.dateTo =  dateTo;
+		this.image =  image;
+		this.url =  url;
+	}
 
-  	createEventInfoElement() {
-  		var listGroup = document.createElement("div");
-  		listGroup.classList.add("list-group-item");
-  		listGroup.classList.add("clearfix");
+	createEventInfoElement() {
+		var listGroup = document.createElement("div");
+		listGroup.classList.add("list-group-item");
+		listGroup.classList.add("clearfix");
 
-  		var profileTeaserLeft = document.createElement("div");
-  		profileTeaserLeft.classList.add("profile-teaser-left");
-  		var profileImage = document.createElement("div");
-  		profileImage.classList.add("profile-img");
-  		var imgProfile = document.createElement("img");
-  		imgProfile.src = this.image;
-  		profileImage.appendChild(imgProfile);
-  		profileTeaserLeft.appendChild(profileImage);
-  		listGroup.appendChild(profileTeaserLeft);
+		var profileTeaserLeft = document.createElement("div");
+		profileTeaserLeft.classList.add("profile-teaser-left");
+		var profileImage = document.createElement("div");
+		profileImage.classList.add("profile-img");
+		var imgProfile = document.createElement("img");
+		if (this.image) {
+			imgProfile.src = this.image;
+		} else {
+			imgProfile.src = "no-img.png";
+		}
+		profileImage.appendChild(imgProfile);
+		profileTeaserLeft.appendChild(profileImage);
+		listGroup.appendChild(profileTeaserLeft);
 
-  		var profileTeaserMain = document.createElement("div");
-  		profileTeaserMain.classList.add("profile-teaser-main");
-  		var profileName = document.createElement("h2");
-  		profileName.classList.add("profile-name");
-  		profileName.innerHTML = "<a href=" + this.url + ">" + this.name + "</a>";
-  		profileTeaserMain.appendChild(profileName);
-  		var profileInfo = document.createElement("div");
-  		profileInfo.classList.add("profile-info");
-  		
-  		var dateInfo = document.createElement("div");
-  		dateInfo.classList.add("info");
-  		dateInfo.innerHTML = "<span>Date: </span> " + this.dateFrom;
-  		profileInfo.appendChild(dateInfo);
+		var profileTeaserMain = document.createElement("div");
+		profileTeaserMain.classList.add("profile-teaser-main");
+		var profileName = document.createElement("h3");
+		profileName.classList.add("profile-name");
+		profileName.innerHTML = "<a target='_blank' href=" + this.url + " >" + this.name + "</a>";
+		profileTeaserMain.appendChild(profileName);
+		var profileInfo = document.createElement("div");
+		profileInfo.classList.add("profile-info");
 
-  		var locationInfo = document.createElement("div");
-  		locationInfo.classList.add("info");
-  		locationInfo.innerHTML = "<span>Location: </span> " + this.location;
-  		profileInfo.appendChild(locationInfo);
+		var dateInfo = document.createElement("div");
+		dateInfo.classList.add("info");
+		dateInfo.innerHTML = "<span>Date: </span> " + this.dateFrom;
+		profileInfo.appendChild(dateInfo);
 
-  		var organizerInfo = document.createElement("div");
-  		organizerInfo.classList.add("info");
-  		organizerInfo.innerHTML = "<span>Organizer: </span> " + this.organizer;
-  		profileInfo.appendChild(organizerInfo);
+		var locationInfo = document.createElement("div");
+		locationInfo.classList.add("info");
+		locationInfo.innerHTML = "<span>Location: </span> " + this.location;
+		profileInfo.appendChild(locationInfo);
 
-  		profileTeaserMain.appendChild(profileInfo);
-  		listGroup.appendChild(profileTeaserMain);
-  		return listGroup;
-  	}
+		var organizerInfo = document.createElement("div");
+		organizerInfo.classList.add("info");
+		organizerInfo.innerHTML = "<span>Organizer: </span> " + this.organizer;
+		profileInfo.appendChild(organizerInfo);
+
+		profileTeaserMain.appendChild(profileInfo);
+		listGroup.appendChild(profileTeaserMain);
+		return listGroup;
+	}
 }
 
+function initSearch(keyWord, fromDate, toDate, location, loadingSuccess, loadingFail) {
+	currPage = 0;
+	currEventBritePage = 0;
+	//always search on eventchain first
+	searchOnEventChain(keyWord, fromDate, toDate, location, 
+		function(evcSuccessResult){
+			cachingRes.concat(evcSuccessResult);
+			if (cachingRes.length < numberEventInOnePage) {
 
-function searchOnEventChain(keyWord, fromDate, toDate) {
+			}
+		}, function(evcError){
 
-	console.log("Start searching on EventChain");
+		}
+	);
+}
 
-	dateFiltering = "";
+function prevPage() {
+
+}
+
+function nextPage(keyWord, fromDate, toDate, location, loadingSuccess, loadingFail) {
+	if ()
+}
+
+function searchOnEventChain(keyWord, fromDate, toDate, location, eventchainSuccess, eventchainFailed) {
+	var dateFiltering = "";
 	if (fromDate) {
 		dateFiltering += " DateFrom: \\\"" + fromDate + "\\\"\\n ";
 	}
@@ -76,103 +103,124 @@ function searchOnEventChain(keyWord, fromDate, toDate) {
 	else {
 		dateFiltering += "";
 	}
-  	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://api.eventchain.io/open",
-		"method": "POST",
-		"headers": {
-		    "content-type": "application/json",
-		    "accept": "*/*",
-		    "cache-control": "no-cache",
+
+	var settings = {
+		url: 'https://api.eventchain.io/open',
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json',
 		},
-		"processData": false,
-		  "data": "{\"query\":\"query{\\n        getEvents(\\n                    SearchParams:{\\n                        KeyWord: \\\"" + keyWord + "\\\"\\n     " + dateFiltering + "                 }\\n                )\\n                {\\n                    ID\\n                    CreateDate\\n                    CreateUserID\\n                    Status\\n                    TaxCalculationType\\n                    InWishList\\n                    BlockchainSecured\\n                    EventInfo {\\n                        Title\\n                        Host\\n                        StartDate\\n\\n                        EndDate: \\n\\n                        Description\\n                        EventImageURL\\n                        TimeZone {\\n                            TZoneID\\n                            FullName\\n                            Name\\n                        }\\n                    }\\n                    EventLocation {\\n                        VenueID\\n                        VenueName\\n                        DisplayMapOnEvent\\n                        Address {\\n                            Address1\\n                            Address2\\n                            City\\n                            CountryID\\n                            CountryName\\n                            ProvinceID\\n                            ProvinceName\\n                            PostalCode\\n                            MapCoords {\\n                                Lat\\n                                Long\\n                                Zoom\\n                            }\\n                        }\\n                    }\\n                    TicketTypes {\\n                        TicketTypeID\\n                        TypeName\\n                        EventID\\n                        Category\\n                        SeatingType\\n                        Price\\n                        Quantity\\n                        Description\\n                        Available\\n                        Currency\\n                        CurrencySign\\n                    }\\n                    ApplicableTaxes {\\n                        EventTaxID\\n                        TaxName\\n                        TaxPercent\\n                    }\\n                }\\n            }\"}"
-	}
+		data: "{\"query\":\"query{\\n        getEvents(\\n                    SearchParams:{\\n                        KeyWord: \\\"" + keyWord + "\\\"\\n     " + dateFiltering + "                 }\\n                )\\n                {\\n                    ID\\n                    CreateDate\\n                    CreateUserID\\n                    Status\\n                    TaxCalculationType\\n                    InWishList\\n                    BlockchainSecured\\n                    EventInfo {\\n                        Title\\n                        Host\\n                        StartDate\\n\\n                        EndDate: \\n\\n                        Description\\n                        EventImageURL\\n                        TimeZone {\\n                            TZoneID\\n                            FullName\\n                            Name\\n                        }\\n                    }\\n                    EventLocation {\\n                        VenueID\\n                        VenueName\\n                        DisplayMapOnEvent\\n                        Address {\\n                            Address1\\n                            Address2\\n                            City\\n                            CountryID\\n                            CountryName\\n                            ProvinceID\\n                            ProvinceName\\n                            PostalCode\\n                            MapCoords {\\n                                Lat\\n                                Long\\n                                Zoom\\n                            }\\n                        }\\n                    }\\n                    TicketTypes {\\n                        TicketTypeID\\n                        TypeName\\n                        EventID\\n                        Category\\n                        SeatingType\\n                        Price\\n                        Quantity\\n                        Description\\n                        Available\\n                        Currency\\n                        CurrencySign\\n                    }\\n                    ApplicableTaxes {\\n                        EventTaxID\\n                        TaxName\\n                        TaxPercent\\n                    }\\n                }\\n            }\"}",
+	};
+	console.log("Start searching on EventChain");
+	console.log(settings);
 
 	$.ajax(settings).done(function (response) {
 		var events = response.data.getEvents;
-	  	for (i = 0; i < events.length; i++) {
-	  		var name = events[i].EventInfo.Title;
-	  		var locationName = events[i].EventLocation.Address.CountryName;
-	  		var organizer = events[i].EventInfo.Host;
-	  		var dateFrom = events[i].EventInfo.StartDate;
-	  		var dateTo = events[i].EventInfo.EndDate;
-	  		var imageUrl = events[i].EventInfo.EventImageURL;
-	  		var url = "";
-	  		
-	  		let event = new EventInfo(name, locationName, organizer, dateFrom, dateTo, imageUrl, url);
-	  		var elem = event.createEventInfoElement();
-	  		document.getElementById("event-list").appendChild(elem);
-	  	}
+		var result = [];
+		for (var i = 0; i < events.length; i++) {
+			var name = events[i].EventInfo.Title;
 
-	  	console.log("Done");
+			var organizer = events[i].EventInfo.Host;
+			var dateFrom = events[i].EventInfo.StartDate;
+			var dateTo = events[i].EventInfo.EndDate;
+			var imageUrl = events[i].EventInfo.EventImageURL;
+
+			var addressObj = events[i].EventLocation.Address;
+
+			var fullAddress = [addressObj.Address1, addressObj.Address2, addressObj.City, addressObj.ProvinceName, addressObj.CountryName].filter(Boolean).join(", ").toLowerCase();
+
+			console.log(fullAddress);
+
+			var locationName = [addressObj.City, addressObj.ProvinceName, addressObj.CountryName].filter(Boolean).join(", ");
+
+			var url = "";
+
+			if (fullAddress.includes(location.toLowerCase())) {
+				let event = new EventInfo(name, locationName, organizer, dateFrom, dateTo, imageUrl, url);
+				result.push(event);
+			}
+			eventchainSuccess(result);
+		}
+
+		console.log("Done");
+	}).fail(function(error) {
+		console.log(error);
+		eventchainFailed(error);
 	});
-  
+
 }
 
-function searchOnEventBrite(keyWord, fromDate, toDate, location = null) {
-	var url = "https://www.eventbriteapi.com/v3/events/search/?token=WY5CK4MUPSE4DQSPEDWQ";
+function searchOnEventBrite(keyWord, fromDate, toDate, location, eventbriteSuccess, eventbriteFailed, page = 0) {
+	var url = "https://www.eventbriteapi.com/v3/events/search/?token=WY5CK4MUPSE4DQSPEDWQ&expand=organizer,venue";
 
-	if (location != null) {
-		url += "&location.within=10km&location.address=" + location; 
+	if (keyWord) {
+		keyWord = keyWord.split(' ').join('+');
+		url += "&q=" + keyWord;
 	}
 
-	if (fromDate != null) {
-		url += "&date_modified.range_start=" + fromDate;
+	if (location) {
+		location = location.split(' ').join('+');
+		url += "&location.address=" + location; 
 	}
 
-	if (toDate != null) {
-		url += "&date_modified.range_end=" + toDate;
+	if (fromDate) {
+		url += "&date_modified.range_start=" + encodeURI(new Date(fromDate).toISOString().substr(0, 19) + "Z");
+	}
+
+	if (toDate) {
+		url += "&date_modified.range_end=" + encodeURI(new Date(toDate).toISOString().substr(0, 19)  + "Z");
+	}
+
+	if (page && page > 0) {
+		url += "&page=" + page;
 	}
 
 	var settings = {
-	  	"async": true,
-	  	"url": url,
-	  	"crossDomain": true,
-	  	"method": "GET",
-	  	"headers": {
-	  		"Authorization": "Bearer WY5CK4MUPSE4DQSPEDWQ",
-	  	}
+		"async": true,
+		"url": url,
+		"crossDomain": true,
+		"method": "GET",
+		"headers": {
+			"Authorization": "Bearer WY5CK4MUPSE4DQSPEDWQ",
+		}
 	}
 
-	$.ajax(settings).done(function (response) {
-	  	console.log(response);
-	  	var events = response.events;
-	  	var okay = true;
-	  	for (i = 0; i < events.length; i++) {
-	  		okay = false;
-	  		var name = events[i].name.text;
-	  		var dateFrom = events[i].start.utc;
-	  		var dateTo = events[i].end.utc;
-	  		var imageUrl;
-	  		if (events[i].logo) {
-	  			imageUrl = events[i].logo.url;
-	  		} else {
-	  			imageUrl = "no-img.png";
-	  		}
-	  		var url = events[i].url;
-	  		var organizer_id = events[i].organizer_id;
-	  		var venue_id = events[i].organizer_id;
-	  		$.ajax({
-	  			//get organizer
-	  			"url": "https://www.eventbriteapi.com/v3/venues/" + venue_id,
-	  			"method": "GET",
-	  			"headers": {
-			  		"Authorization": "Bearer WY5CK4MUPSE4DQSPEDWQ",
-			  		"Content-Type": "application/x-www-form-urlencoded"
-			  	}
-	  		}).done(function(venueRes) {
-	  			okay = true;
-	  			var locationName = venueRes.name;
-	  			var organizer = "organizerRes.name";	
-	  			let event = new EventInfo(name, locationName, organizer, dateFrom, dateTo, imageUrl, url);
-	  			var elem = event.createEventInfoElement();
-	  			document.getElementById("event-list").appendChild(elem);
-	  		});
-	  	}
-
-	  	console.log("Done");
-	});
+	$.ajax(settings)
+		.done(function (response) {
+			console.log(response);
+			var result = [];
+			var events = response.events;
+			for (let i = 0; i < events.length; i++) {
+				var name = events[i].name.text;
+				var dateFrom = events[i].start.utc;
+				var dateTo = events[i].end.utc;
+				var imageUrl;
+				if (events[i].logo) {
+					imageUrl = events[i].logo.url;
+				} else {
+					imageUrl = "no-img.png";
+				}
+				var url = events[i].url;
+				var organizer;
+				if (events[i].organizer) {
+					organizer = events[i].organizer.name;
+				} else {
+					organizer = "undefined";
+				}
+				var locationName;
+				if (events[i].venue) {
+					locationName = events[i].venue.name;
+				} else {
+					locationName = "undefined";
+				}
+				let event = new EventInfo(name, locationName, organizer, dateFrom, dateTo, imageUrl, url);
+				result.push(event);
+			}
+			eventbriteSuccess(result);
+		})
+		.fail(function(err) {
+			eventbriteFailed(err);
+		});
 
 }
